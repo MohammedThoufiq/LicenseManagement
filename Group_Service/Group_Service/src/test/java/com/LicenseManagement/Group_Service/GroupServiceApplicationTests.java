47148ae7e6d2a7ac23919b3ed002c24d63d7876e
shiva <shiva.kailash06@gmail.com>
2023-03-15 19:16:42 +0530

    Initial Commit

---
+++ Group_Service/Group_Service/src/test/java/com/LicenseManagement/Group_Service/GroupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LicenseManagement.Group_Service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
